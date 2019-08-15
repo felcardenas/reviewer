8160076f815f1cd7a31cd17c8ff0f7cc052ceeca
felcardenas <fel_cardenas@outlook.com>
2019-08-15 15:07:25 -0400

    webservice login optimizado

--- global/conexion.php
+++ global/conexion.php
@@ -1,12 +1,5 @@
 <?php 
 $conexion = mysqli_connect("localhost","root","","pruebasreviewer");
 
-// error_reporting(0);
-
-// if($conexion){
-//     //echo "ok";
-// }else{
-//     //echo "PROBLEMA EN LA CONEXION";
-// }
 
 ?>
--- webservices/iniciosesion.php
+++ webservices/iniciosesion.php
@@ -1,17 +1,94 @@
 <?php
 include "../global/conexion.php";
 
+
+if(isset($_POST)){
+    validarUsuario($conexion);
+}
+
+
+function validarUsuario($conexion){
+    $usuario = $_POST['usuario'];
+    $sql = "SELECT nombre FROM usuarios WHERE nombre = '$usuario'";
+    
+    $resultado = mysqli_query($conexion,$sql);
+    $filas = mysqli_num_rows($resultado);
+
+     if($filas>0){
+        validarPass($usuario,$conexion);
+     }else{
+        validarCorreo($usuario,$conexion);
+     }
+}
+
+function validarCorreo($usuario,$conexion){
+    $sql = "SELECT nombre, correo FROM usuarios WHERE correo = '$usuario'";
+        
+    $resultado = mysqli_query($conexion,$sql);
+    $filas = mysqli_num_rows($resultado);
+        
+            
+        if($filas>0){
+            validarPass($usuario,$conexion);
+        }else{
+            header("Location:../iniciosesion.php?id=1");
+        }
+}
+
+function validarPass($usuario,$conexion){
+    
+    $pass = $_POST['pass'];
+    $sql = "SELECT pass FROM usuarios WHERE pass = '$pass'";
+    
+    $resultado = mysqli_query($conexion,$sql);
+    $filas = mysqli_num_rows($resultado);
+
+        if($filas>0){
+            session_start();
+            $_SESSION['usuario'] = $usuario;
+            header("Location:../index.php");
+        }else{
+            header("Location:../iniciosesion.php?id=2");
+        }
+}
+
+
+
+
+/*
 if(isset($_POST)){
     
     $usuario = $_POST['usuario'];
     $sql = "SELECT nombre FROM usuarios WHERE nombre = '$usuario'";
-
+    
     $resultado = mysqli_query($conexion,$sql);
     $filas = mysqli_num_rows($resultado);
 
-
      if($filas>0){
+        validarPass($usuario,$conexion);
+      
+     }else{
+        $usuario = $_POST['usuario'];
+        $sql = "SELECT nombre, correo FROM usuarios WHERE correo = '$usuario'";
+        
+        
+        $resultado = mysqli_query($conexion,$sql);
+        $filas = mysqli_num_rows($resultado);
+        
             
+            if($filas>0){
+                validarPass($usuario,$conexion);
+               
+            }else{
+                header("Location:../iniciosesion.php?id=1");
+            }
+
+     }
+
+}*/
+/*
+function validarPass($usuario,$conexion){
+    
                 $pass = $_POST['pass'];
                 $sql = "SELECT pass FROM usuarios WHERE pass = '$pass'";
                 
@@ -19,43 +96,11 @@ if(isset($_POST)){
                 $filas = mysqli_num_rows($resultado);
 
                     if($filas>0){
-                        
                         session_start();
                         $_SESSION['usuario'] = $usuario;
                         header("Location:../index.php");
                     }else{
                         header("Location:../iniciosesion.php?id=2");
                     }
-
-     }else{
-        
-        $usuario = $_POST['usuario'];
-        $sql = "SELECT nombre, correo FROM usuarios WHERE correo = '$usuario'";
-        
-        $resultado = mysqli_query($conexion,$sql);
-        $filas = mysqli_num_rows($resultado);
-
-            if($filas>0){
-                
-                    $pass = $_POST['pass'];
-                    $sql = "SELECT pass FROM usuarios WHERE pass = '$pass'";
-                    
-                    $resultado = mysqli_query($conexion,$sql);
-                    $filas = mysqli_num_rows($resultado);
-            
-                        if($filas>0){
-                            session_start();
-                            $_SESSION['usuario'] = $usuario;
-                            header("Location:../index.php");
-                        }else{
-                            header("Location:../iniciosesion.php?id=2");
-                        }
-        
-            }else{
-                header("Location:../iniciosesion.php?id=1");
-            }
-
-     }
-
-}
+}*/
 ?>
